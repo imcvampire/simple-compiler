c1d2f8c6ff3692d60512843b38c6fa0eb5d5e200
imcvampire <anhnq0@gmail.com>
2024-02-15 22:25:06 +0200

    feat: update

--- check.sh
+++ check.sh
@@ -4,4 +4,4 @@ cd "$(dirname "${0}")"
 poetry run mypy .
 rm -Rf test_programs/workdir
 poetry run coverage run -m pytest -vv tests/
-poetry run coverage report -m
+poetry run coverage report -m --omit="tests/*"
