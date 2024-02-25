e648d78a7ada9c9e6c32cd6269126158d393d2f3
imcvampire <anhnq0@gmail.com>
2024-02-25 23:24:16 +0200

    feat: update

--- src/compiler/type.py
+++ src/compiler/type.py
@@ -10,8 +10,22 @@ class Type:
 class PrimitiveType(Type):
     name: str
 
+@dataclass(frozen=True)
+class ConstType(Type):
+    name: str
+
+    def __eq__(self, other: object):
+        if isinstance(other, ConstType):
+            return self.name == other.name
+        elif isinstance(other, PrimitiveType):
+            return self.name == other.name
+
+        return False
 
 Int = PrimitiveType("Int")
 Bool = PrimitiveType("Bool")
 Unit = PrimitiveType("Unit")
 Func = PrimitiveType("Function")
+
+ConstInt = ConstType("Int")
+ConstBool = ConstType("Bool")
